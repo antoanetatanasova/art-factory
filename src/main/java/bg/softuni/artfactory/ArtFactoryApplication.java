aea998325491a73686a26a1d09bea6d934f3906d
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2020-07-19 16:41:35 +0300

    Resources added.

---
+++ src/main/java/bg/softuni/artfactory/ArtFactoryApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.artfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtFactoryApplication.class, args);
+	}
+
+}
