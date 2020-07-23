84f49de4aa3eed73a205bede726421a795cdc287
Antoaneta Atanasova <antoanetatanasova@gmail.com>
2020-07-23 19:28:31 +0300

    Adding SecurityConfig and Stats.

---
+++ src/main/java/bg/softuni/artfactory/config/PasswordConfig.java
@@ -0,0 +1,16 @@
+package bg.softuni.artfactory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+  @Bean
+  public PasswordEncoder createPasswordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+}
---
+++ src/main/java/bg/softuni/artfactory/config/WebConfig.java
@@ -0,0 +1,21 @@
+package bg.softuni.artfactory.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import bg.softuni.artfactory.stats.StatsInterceptor;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+    private StatsInterceptor statsInterceptor;
+
+    public WebConfig(StatsInterceptor statsInterceptor) {
+        this.statsInterceptor = statsInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(statsInterceptor);
+    }
+}
